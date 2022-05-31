1d24ee7cbbef7d704656baddc34fac86558480ac
dancohen2022 <102294228+dancohen2022@users.noreply.github.com>
2022-05-31 08:13:23 +0300

    Update main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -16,10 +16,19 @@ func main() {
 	json := "hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&M=on&s=on"
 
 	routes.GetSynagogueHttpJson(link, json)
+
 	/*
-		r := mux.NewRouter()
-		routes.RegisterBetknessetRoutes(r)
-		http.Handle("/", r)
-		log.Fatal(http.ListenAndServe("localhost:9010", r))
+		http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+			key := "q"
+			val := req.URL.Query().Get(key)
+			io.WriteString(res, "Do my search:"+val)
+		})
+		/*
+
+		/*
+			r := mux.NewRouter()
+			routes.RegisterBetknessetRoutes(r)
+			http.Handle("/", r)
+			log.Fatal(http.ListenAndServe("localhost:9010", r))
 	*/
 }
