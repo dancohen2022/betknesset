7201327ab3e21d379a0326c8e25a2e5281282f42
dancohen2022 <102294228+dancohen2022@users.noreply.github.com>
2022-05-31 07:59:44 +0300

    Update

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/dancohen2022/betknesset/pkg/models"
+	"github.com/dancohen2022/betknesset/pkg/routes"
 )
 
 func main() {
@@ -10,6 +11,11 @@ func main() {
 	//
 
 	models.CreateDir("test")
+
+	link := "https://www.hebcal.com/"
+	json := "hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=x&ss=on&mf=on&c=on&geo=geoname&geonameid=3448439&M=on&s=on"
+
+	routes.GetSynagogueHttpJson(link, json)
 	/*
 		r := mux.NewRouter()
 		routes.RegisterBetknessetRoutes(r)
--- pkg/routes/betknesset-routes.go
+++ pkg/routes/betknesset-routes.go
@@ -1,6 +1,10 @@
 package routes
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	"github.com/dancohen2022/betknesset/pkg/controllers"
 	"github.com/gorilla/mux"
 )
@@ -8,4 +12,24 @@ import (
 var RegisterBetknessetRoutes = func(router *mux.Router) {
 	router.HandleFunc("/betknesset/{name, key}", controllers.GetDayTimes).Methods("GET")
 
+}
+
+func GetSynagogueHttpJson(link, json string) string {
+	//resp, err := http.Get("http://gobyexample.com")
+	resp, err := http.Get(fmt.Sprint(link, json))
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	//Print the HTTP response status.
+
+	fmt.Println("Response status:", resp.Status)
+	//Print the first 5 lines of the response body.
+
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Printf("Body : %s", body)
+	if err != nil {
+		fmt.Printf("Error %s", err)
+	}
+	return string(body)
 }
