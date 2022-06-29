3669ee95c34e4186fb503f8e55e8552d44c520b9
dancohen2022 <102294228+dancohen2022@users.noreply.github.com>
2022-06-29 18:05:42 +0300

    Update

--- main/main.go
+++ main/main.go
@@ -4,8 +4,10 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"sync"
 
 	"github.com/dancohen2022/betknesset/pkg/mdb"
+	"github.com/dancohen2022/betknesset/pkg/synagogues"
 	"github.com/mattn/go-sqlite3"
 )
 
@@ -32,8 +34,54 @@ func main() {
 	// close database connection before exiting program.
 	defer db.Close()
 	////// CREATE TABLES if first time
-	mdb.CreateDBTables(db)
+	var wg sync.WaitGroup
+	wg.Add(1)
 
+	go func() {
+		defer wg.Done()
+		mdb.CreateDBTables(db)
+	}()
+	wg.Wait()
+
+	u := synagogues.User{
+		Id:       0,
+		Name:     "Dan",
+		Key:      "12345",
+		UserType: "manager",
+		Active:   true,
+	}
+
+	/*
+		User        User   `json:"user"`
+		CalendarApi string `json:"calendar"`
+		ZmanimApi   string `json:"zmanim"`
+		Config      string `json:"config"`
+		Logo        string `json:"logo"`
+		Background  string `json:"background"`
+	*/
+
+	s := synagogues.Synagogue{
+		User: synagogues.User{
+			Id:       0,
+			Name:     "shuva_raanana",
+			Key:      "123456",
+			UserType: "synagogue",
+			Active:   true,
+		},
+		CalendarApi: "https://www.hebcal.com/hebcal?v=1&cfg=json&maj=on&min=on&mod=on&nx=on&year=now&month=6&ss=on&mf=on&c=on&geo=geopos&latitude=32.1848&longitude=34.8713&tzid=Israel&M=on&s=on",
+		ZmanimApi:   "https://www.hebcal.com/zmanim?cfg=json&geo=geopos&latitude=32.1848&longitude=34.8713&tzid=Israel&year=now",
+		Config:      "",
+		Logo:        "",
+		Background:  "",
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		mdb.CreateManager(db, u)
+		mdb.CreateSynagogue(db, s)
+	}()
+	wg.Wait()
 	/////
 	//Init from files
 	//functions.InitSynagogues()
